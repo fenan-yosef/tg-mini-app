c18c13d1bf85980fa880184551ff5539eb3586de
pal-oe <fenanyosef@gmail.com>
2024-11-27 10:16:33 +0300

    going on

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
-import Footer from "./Footer";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Task Manager",
+  title: 'Next.js Bottom Navbar',
+  description: 'Demo of a bottom navbar in Next.js 14',
 };
 
 export default function RootLayout({ children }) {
@@ -11,10 +14,32 @@ export default function RootLayout({ children }) {
       <head>
         <script src="https://telegram.org/js/telegram-web-app.js?56"></script>
       </head>
-      <body>
-        <main style={{ minHeight: "calc(100vh - 4rem)" }}>{children}</main>
-        <Footer />
+      <body className={`${inter.className} bg-gray-900 text-white`}>
+        <div className="min-h-screen flex flex-col">
+          <main className="flex-1">{children}</main>
+          <nav className="fixed bottom-0 left-0 p-3 w-full bg-gray-800 shadow-md border-t rounded-tl-3xl rounded-tr-3xl border-gray-700">
+            <div className="flex justify-around items-center p-3">
+              <NavLink href="/" icon='faHome' label="Home" />
+              <NavLink href="/tasks" icon="fa-tasks" label="Tasks" />
+              <NavLink href="/new-task" icon="faUser" label="New" />
+              <NavLink href="/projects" icon="faUser" label="Projects" />
+            </div>
+          </nav>
+        </div>
       </body>
     </html>
+  );
+}
+
+function NavLink({ href, icon, label }) {
+  return (
+    <a
+      href={href}
+      className="flex flex-col items-center text-sm hover:text-teal-400 transition duration-300"
+    >
+      <i className={`fas ${icon} text-xl mb-1`}></i>
+      {/* <FontAwesomeIcon icon={icon} size="sm" /> */}
+      {label}
+    </a>
   );
 }
--- src/app/new-task/page.js
+++ src/app/new-task/page.js
@@ -1,9 +1,8 @@
 export default function TasksPage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Tasks</h1>
-        <p>Here you can manage your tasks...</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">New</h1>
+      <p>You add here</p>
+    </div>
+  );
+}
